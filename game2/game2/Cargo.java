ad9f860bbfaab2a7a3567569c873173744afaa45
jsonius <j.sonius@live.nl>
2016-11-28 19:24:31 +0100

    Add files via upload
    Random Cargo Implemented

---
+++ game2/game2/Cargo.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class cargo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cargo extends Actor
+{
+    
+    protected int weight;
+    protected String color;
+    
+    public Cargo()
+    {
+        GreenfootImage image = getImage();  
+        image.scale(80, 35);
+        setImage(image);
+    }
+    
+    /**
+     * Act - do whatever the cargo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    public int getWeight() {
+        return weight;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+}
