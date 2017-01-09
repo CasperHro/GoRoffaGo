e1491bcf6c1a47c2e21ad287b82e62de15682320
jsonius <j.sonius@live.nl>
2017-01-09 10:05:41 +0100

    Add files via upload

---
+++ G4_Complete.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+
+/**
+ * Write a description of class G4_Complete here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class G4_Complete extends Actor
+{
+    
+    /**
+     * Act - do whatever the G4_Complete wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        
+    } 
+    
+    
+}
