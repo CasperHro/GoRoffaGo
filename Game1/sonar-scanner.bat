d11a77927ba022caeb6f4a585b1d1b12de9ed52e
CasperHro <0931371@hr.nl>
2017-01-10 00:04:04 +0100

    edit sonarscanner scripts
    Edited sonarscanner startscripts for windows and macosx/linux

--- Game1/sonar-scanner.bat
+++ Game1/sonar-scanner.bat
@@ -4,20 +4,21 @@ for %%* in (.) do set CurrDirName=%%~nx*
 if exist ..\sonar-scanner-2.8\bin\sonar-scanner.bat (
 
     ..\sonar-scanner-2.8\bin\sonar-scanner.bat
-    start "" http://localhost:9000/dashboard?id=%CurrDirName%
+    start "" http://localhost:9000/dashboard/index?id=%CurrDirName%
 
 ) else (
     cls
     echo.
-    echo Download SonarQube and Sonar-scanner and extract the zips in de folder:
-    echo   C:\tmp_install\
+    echo Kan sonarscanner niet vonden. Zorg dat de sonarQube en sonarscanner
+    echo in de rootmap van het project staan.
     echo.
     echo Download SonarQube op: http://www.sonarqube.org/
     echo Download SonarQube Scanner van:
     echo http://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner
     echo.
-    echo Start daarna eerst SonarQube met StartSonar.bat uit de bin map en
-    echo controleer op http://localhost:9000/ of de server draait.
+    echo Start daarna eerst SonarQube met start-sonarqube.bat wacht tot 
+    echo "SonarQube is up" wordt weergegeven en controleer op
+    echo http://localhost:9000/ of de server draait.
     echo.
     echo Dan dit batch bestand opnieuw gestart worden.
     echo.
